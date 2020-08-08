6585b71f897699131be7fb9824d56a3236b3f118
Gustavo Murillo Vega <43526958+noire0@users.noreply.github.com>
2020-08-08 13:30:54 -0600

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-gfortran example.f -o example.bin
-gfortran array-slicing-example.f -o array-slicing-example.bin
+mkdir bin
+gfortran example.f -o bin/example.bin
+gfortran array-slicing-example.f -o bin/array-slicing-example.bin
+gfortran coefficients-of-eigenpolynomial.f -o bin/coefficients-of-eigenpolynomial.bin
